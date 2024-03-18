0960659b89f9d4e4617bf5d1805b7accfa24a29d
ReasonXI <162834909+ReasonXI@users.noreply.github.com>
2024-03-18 23:04:04 +0100

    display ash qty of soot sack

--- include/strings.h
+++ include/strings.h
@@ -3034,4 +3034,7 @@ extern const u8 gText_BoxName[];
 extern const u8 gText_PkmnsNickname[];
 extern const u8 gText_TellHimTheWords[];
 
+// Soot Bag
+extern const u8 gText_AshQty[];
+
 #endif // GUARD_STRINGS_H
